f112e7fcd83696ba2ba38a361116824c61604729
Leon-Africa <mr.leon.africa@gmail.com>
2021-07-27 13:53:18 +0200

    syntax

--- src/commands/pa.ts
+++ src/commands/pa.ts
@@ -1,11 +1,10 @@
-import Command from '@oclif/command'
-import { string } from '@oclif/command/lib/flags';
-import cli from 'cli-ux'
-import {exec} from "shelljs"
+import Command from '@oclif/command';
+import cli from 'cli-ux';
+import {exec} from "shelljs";
 
 
 export class PA extends Command {
-  static description = 'commit and push all changes'
+  static description = 'commit and push all changes';
 
   
 
@@ -24,38 +23,38 @@ export class PA extends Command {
 
     current_dir = current_dir.substring(0,current_dir.length - 3);
 
-    current_dir += '/"'
+    current_dir += '/"';
 
-    let result = exec(`git -C ${current_dir} rev-parse`)
+    let result = exec(`git -C ${current_dir} rev-parse`);
 
     if(result.code) {
-      this.error('Please first configure git i.e \n git config --global user.name "your name" \n git config --global user.email "your email\n Also ensure that you set the git credentials for your user \n https://git-scm.com/book/en/v2/Git-Tools-Credential-Storage')
+      this.error('Please first configure git i.e \n git config --global user.name "your name" \n git config --global user.email "your email\n Also ensure that you set the git credentials for your user \n https://git-scm.com/book/en/v2/Git-Tools-Credential-Storage');
       }
 
     // prompt for commit message
-    let commit_message = await cli.prompt('Enter your commit message')
+    let commit_message = await cli.prompt('Enter your commit message');
 
-    console.log(commit_message)
+    console.log(commit_message);
 
     commit_message = JSON.stringify(commit_message);
 
     if(commit_message === "" || commit_message === " " || commit_message === null){
-      this.error('Error: Git commit message is required \n Please enter a commit message relevant to this commit')
+      this.error('Error: Git commit message is required \n Please enter a commit message relevant to this commit');
     }
 
     //run the command
     try{
      exec(`git commit -a -m ${commit_message} && git push`);   
-     this.error('Succesfully uploaded to remote Repo using Git. NOT AN ERROR')
+     this.error('Succesfully uploaded to remote Repo using Git. NOT AN ERROR');
     }
     catch (e: unknown){ 
       if (typeof e === "string") {
-         this.error(`${e.toUpperCase}`)
+         this.error(`${e.toUpperCase}`);
       } else if (e instanceof Error) {
-        this.error(`${e.message}`)
+        this.error(`${e.message}`);
       }
 
-      this.error('Error: Git commit and push failed')
+      this.error('Error: Git commit and push failed');
   }
 
       
